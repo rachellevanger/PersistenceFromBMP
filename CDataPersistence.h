45cf09f8b7dfaa6cb5f700158e8c92327be11652
Rachel Levanger <rachel.levanger@gmail.com>
2015-11-11 17:28:33 -0500

    Fixing header file to add filter.

--- CDataPersistence.h
+++ CDataPersistence.h
@@ -25,8 +25,8 @@ public:
 	virtual ~CDataPersistence();
 
 	void LoadData( const char* file_name );
-	void SavePersistenceDiagrams( const char* save_as, const int isRadial, const int center_x, const int center_y, const int radius );
-	void SavePersistenceDiagramsInvers( const char* save_as, const int isRadial, const int center_x, const int center_y, const int radius  );
+	void SavePersistenceDiagrams( const char* save_as, const int isRadial, const int center_x, const int center_y, const int radius, const int filter );
+	void SavePersistenceDiagramsInvers( const char* save_as, const int isRadial, const int center_x, const int center_y, const int radius, const int filter  );
 };
 
 #endif /* CDATAPERSISTENCE_H_ */
